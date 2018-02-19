bbc2e9da8a5b5c5b5a0930545c21d08496864525
Alexey Golovnya <alexey.golovnya@gmail.com>
2018-02-19 22:24:12 +0700

    Test handler as middleware

--- tests/ReaconTest.php
+++ tests/ReaconTest.php
@@ -57,4 +57,65 @@ class ReaconTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf(MiddlewareInterface::class, $reacon);
     }
+
+    public function testShouldBeUsedAsMiddleware()
+    {
+        $request = $this->createMock(ServerRequestInterface::class);
+
+        $middleware1 = $this->createMock(MiddlewareInterface::class);
+        $middleware1->expects($this->once())
+            ->method("process")
+            ->willReturnCallback(
+                function(
+                    \Psr\Http\Message\ServerRequestInterface $request,
+                    RequestHandlerInterface $handler
+                ) {
+                    return $handler->handle($request);
+                }
+            );
+
+        $middleware2 = $this->createMock(MiddlewareInterface::class);
+        $middleware2->expects($this->once())
+            ->method("process")
+            ->with($request)
+            ->willReturnCallback(
+                function(
+                    \Psr\Http\Message\ServerRequestInterface $request,
+                    RequestHandlerInterface $handler
+                ) {
+                    return $this->createMock(ResponseInterface::class);
+                }
+            );
+
+        $reacon1 = new Reacon(
+            [
+                $middleware1,
+                $middleware2,
+            ]
+        );
+
+        $middleware3 = $this->createMock(MiddlewareInterface::class);
+        $middleware3->expects($this->once())
+            ->method("process")
+            ->with($request)
+            ->willReturnCallback(
+                function(
+                    \Psr\Http\Message\ServerRequestInterface $request,
+                    RequestHandlerInterface $handler
+                ) {
+                    return $handler->handle($request);
+                }
+            );
+
+        $reacon2 = new Reacon(
+            [
+                $middleware3,
+                $reacon1,
+            ]
+        );
+
+        $response = $reacon2->handle($request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
 }
